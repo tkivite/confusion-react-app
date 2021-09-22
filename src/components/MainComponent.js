384cba8d0261916b8b41a92ebeebe7ba118ec35f
tkivite <tkivite@gmail.com>
2021-09-23 00:35:52 +0300

    updated home and contactus pages

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,18 +2,25 @@ import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
-import Menu from "./MenuComponent";
 import Home from "./HomeComponent";
+import Menu from "./MenuComponent";
+import Contact from "./ContactComponent";
 import DishDetail from "./DishdetailComponent";
-import { DISHES } from "../shared/dishes";
 import { Switch, Route, Redirect } from "react-router-dom";
+import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 
 class Main extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
@@ -23,7 +30,13 @@ class Main extends Component {
 
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
     };
 
     return (
@@ -31,11 +44,13 @@ class Main extends Component {
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
+
           <Route
             exact
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
